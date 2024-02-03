b069111d57f64332fbcaad40005c0aaa53217b3a
ylzhang828 <14034463+ylzhang828@user.noreply.gitee.com>
2024-02-04 06:39:59 +0800

    添加了Movielens-1m数据集(文本)的支持

---
+++ main_vbpr_ml.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+# @file  :main.py.py
+# @time  :2024/02/01
+# @author:ylZhang
+import os
+import torch
+from torch import optim
+from vbpr import VBPR, Trainer
+# from vbpr.datasets import TradesyDataset
+from vbpr.datasets import MovielensDataset
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+dataset, features = MovielensDataset.from_files(
+    "data/ml-1m/ml-1m-ui.json",
+    "data/ml-1m/ml-1m_text_feats.npy",
+)
+print("用户数：", dataset.n_users)
+print("物品数：", dataset.n_items)
+
+model = VBPR(
+    dataset.n_users,
+    dataset.n_items,
+    torch.tensor(features, dtype=torch.float32),
+    dim_gamma=64,
+    dim_theta=64,
+).to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+
+optimizer = optim.Adam(model.parameters(), lr=5e-04)
+
+trainer = Trainer(model, optimizer)
+trainer.fit(dataset, n_epochs=500)
+
+
+
